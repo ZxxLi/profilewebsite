332b191877c3b86c39c5b395855c9722d7753f0c
Xing <zli358@sheffield.ac.uk>
2026-07-13 19:15:45 +0100

    Cleared fields when field entry was successful, also ensured that email service displayed all information correctly

--- portfolio-website/src/components/Form.jsx
+++ portfolio-website/src/components/Form.jsx
@@ -1,11 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 export const Form = () => {
-  const form = useRef();
+    const form = useRef();
 
-  const sendEmail = (e) => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+    
+    const sendEmail = (e) => {
     e.preventDefault();
+    
+    /*Ensure that the following form keys are set up correctly*/
 
     emailjs
       .sendForm('servicekey', 'templatekey', form.current, {
@@ -14,6 +22,13 @@ export const Form = () => {
       .then(
         () => {
           console.log('SUCCESS!');
+
+          setFormData({
+            name: '',
+            email: '',
+            message: ''
+        });
+
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -24,11 +39,23 @@ export const Form = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input 
+        type="text" 
+        name="name" 
+        value={formData.name}
+        onChange={(e) => setFormData({...formData, name: e.target.value})}/>
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input 
+        type="email" 
+        name="email"
+        value={formData.email}
+        onChange={(e) => setFormData({...formData, email: e.target.value})}/>
       <label>Message</label>
-      <textarea name="message" />
+      <textarea 
+        type="text"
+        name="message"
+        value={formData.message}
+        onChange={(e) => setFormData({...formData, message: e.target.value})}/>
       <input type="submit" value="Send" />
     </form>
   );
